28f71373dc384c251acc6b72c89377ab7fec716b
Malachi Griffie <malachi@nexussays.com>
2014-09-25 07:07:44 -0700

    Added files and updated license and readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-load.js-dist
-============
+This repository contains the distributable files of [load.js](https://github.com/nexussays/load.js), primarily for use with bower.
+
+Actual repository is located here: https://github.com/nexussays/load.js
---
+++ load.d.ts
@@ -0,0 +1,31 @@
+﻿export = load;
+
+declare function load(file: string): load.Promise;
+declare function load(fileWithLabel: any): load.Promise;
+declare function load(...files: any[]): load.Promise;
+
+declare module load
+{
+   interface Promise
+   {
+      then(callback: () => void): Promise;
+      then(file: string): Promise;
+      then(fileWithLabel: any): Promise;
+      then(...files: any[]): Promise;
+   }
+
+   function setBaseUrl(path: string): void;
+
+   function getBaseUrl(): string;
+
+   function setQuerystring(args: string): void;
+
+   function when(dependency: string, callback: () => void, waiting?: (missing: string[]) => void): void;
+   function when(dependencies: string[], callback: () => void, waiting?: (missing: string[]) => void): void;
+   function when(dependency: string, file: string, waiting?: (missing: string[]) => void): void;
+   function when(dependencies: string[], file: string, waiting?: (missing: string[]) => void): void;
+   function when(dependency: string, files: string[], waiting?: (missing: string[]) => void): void;
+   function when(dependencies: string[], files: string[], waiting?: (missing: string[]) => void): void;
+   function when(dependency: string, fileWithLabel: any, waiting?: (missing: string[]) => void): void;
+   function when(dependencies: string[], fileWithLabel: any, waiting?: (missing: string[]) => void): void;
+}
