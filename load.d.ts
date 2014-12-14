b7aa36445ef00200a805a2f7e75333f849d4a230
Malachi Griffie <malachi@nexussays.com>
2014-12-13 22:24:35 -0800

    version bump

--- load.d.ts
+++ load.d.ts
@@ -1,7 +1,9 @@
 ﻿export = load;
 
 declare function load(file: string): load.Promise;
+
 declare function load(fileWithLabel: any): load.Promise;
+
 declare function load(...files: any[]): load.Promise;
 
 declare module load
@@ -21,11 +23,18 @@ declare module load
    function setQuerystring(args: string): void;
 
    function when(dependency: string, callback: () => void, waiting?: (missing: string[]) => void): void;
+
    function when(dependencies: string[], callback: () => void, waiting?: (missing: string[]) => void): void;
+
    function when(dependency: string, file: string, waiting?: (missing: string[]) => void): void;
+
    function when(dependencies: string[], file: string, waiting?: (missing: string[]) => void): void;
+
    function when(dependency: string, files: string[], waiting?: (missing: string[]) => void): void;
+
    function when(dependencies: string[], files: string[], waiting?: (missing: string[]) => void): void;
+
    function when(dependency: string, fileWithLabel: any, waiting?: (missing: string[]) => void): void;
+
    function when(dependencies: string[], fileWithLabel: any, waiting?: (missing: string[]) => void): void;
 }
